0465c9d19e9b111aecd254549de1273439f37856
SargisHak0byan <sargis.hakobyan4@edu.ysu.am>
2023-11-30 16:05:37 +0400

    last version not for gpu

--- CardFieldsDetection.py
+++ CardFieldsDetection.py
@@ -1,152 +1,63 @@
-import torch
-import numpy as np
 import os
-import cv2
-import pandas as pd
-import json
-from PIL import Image
-from torch.utils.data import Dataset
-from torchvision.transforms import Resize
+import torch
 
-from data_aug.data_aug import *
-
-IMAGE_SIZE = 1024
+from torchvision.io import read_image
+from torchvision.ops.boxes import masks_to_boxes
+from torchvision import tv_tensors
+from torchvision.transforms.v2 import functional as F
 
 
-
-class CardFieldsDetection(Dataset):
-    def __init__(self, data_path, csv_path, is_Train=True, transform=None):
-        self.data = []
-        self.annotations = []
-        self.obj_labels = []
-        self.masks = []
-        self.transform = transform
-
-        # defining directory to store augmented images
-        class_name = data_path.split("/")[-1]
-        aug_root = "Dataset"
-        aug_path = os.path.join(aug_root, class_name)
-        aug_image_id = 102
-
-        # open annotation files
-        labels_1_50 = pd.read_csv(os.path.join(csv_path, "labeled_1_50.csv"))
-        labels_51_101 = pd.read_csv(os.path.join(csv_path, "labeled_51_101.csv"))
-        labels_102_125 = pd.read_csv(os.path.join(csv_path, "new_brit_ID_ant.csv"))
-
-        # get bbox values
-        labels_1_50['region_shape_attributes'] = labels_1_50['region_shape_attributes'].apply(lambda x: json.loads(x))
-        labels_51_101['region_shape_attributes'] = labels_51_101['region_shape_attributes'].apply(
-            lambda x: json.loads(x))
-        labels_102_125['region_shape_attributes'] = labels_102_125['region_shape_attributes'].apply(
-            lambda x: json.loads(x))
-
-        for filename in os.listdir(data_path):
-            image_id = int(filename.split(".")[0])
-            image_path = os.path.join(data_path, filename)
-            region_attributes = None
-            if image_id <= 125:
-                # find annotation of original images in the csv
-                if image_id < 51:
-                    region_attributes = labels_1_50[labels_1_50.filename == filename]['region_shape_attributes']
-                elif 51 <= image_id <= 101:
-                    region_attributes = labels_51_101[labels_51_101.filename == filename]['region_shape_attributes']
-                if not is_Train and 101 <= image_id <= 125:
-                    region_attributes = labels_102_125[labels_102_125.filename == filename]['region_shape_attributes']
-
-                    # read image
-                img = cv2.imread(image_path)[:, :, ::-1]  # OpenCV uses BGR channels
-                height, width, channels = img.shape
-
-                # convert annotations to xmin xman ymin ymax format
-                annotation = []
-                for idx, item in enumerate(region_attributes):
-                    x1 = item['x']
-                    x2 = x1 + item['width']
-
-                    y1 = item['y']
-                    y2 = y1 + item['height']
-
-                    annotation.append([x1, y1, x2, y2, idx + 1])
-
-                    # skip unannotated images
-                if len(annotation) == 0:
-                    continue
-
-                # resize image and its bboxes
-                resizer = Resize(IMAGE_SIZE)
-                img, annotation = resizer(img, np.array(annotation, dtype='float'))
-                path = aug_path + f"/{image_id}.jpg" if is_Train else aug_root + f"/val/{image_id}.jpg"
-                cv2.imwrite(path, img)
-
-                # save required information
-                self.data.append(path)
-                self.annotations.append([x[:-1] for x in annotation])
-                self.obj_labels.append([x[-1] for x in annotation])
-
-                if is_Train:
-                    # define augmentations from external library
-                    transforms = [
-                        RandomHorizontalFlip(1),
-                        RandomScale(0.3, diff=True),
-                        RandomTranslate(0.3, diff=True),
-                        RandomRotate(30), RandomRotate(60), RandomRotate(90), RandomRotate(120),
-                        RandomRotate(180),
-                        RandomShear(0.2),
-                        RandomHSV(100, 100, 100),
-                    ]
-
-                    # augment image and its bboxes accordingly
-                    for transform in transforms:
-                        aug_image_path = aug_path + f"/{aug_image_id}.jpg"
-                        aug_image_id += 1
-
-                        annotation = np.array(annotation, dtype='float')
-                        aug_img, aug_annotation = transform(img.copy(), annotation.copy())
-                        try:
-                            cv2.imwrite(aug_image_path, aug_img)  # save augmented image
-                            # save required information
-                            self.data.append(aug_image_path)
-                            self.annotations.append([x[:-1] for x in aug_annotation])
-                            self.obj_labels.append([x[-1] for x in aug_annotation])
-                        except:
-                            continue
-
-        print(f"[Dataset/__init__] {len(self.data)} images.")
+class CardFieldsDetection(torch.utils.data.Dataset):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+        # load images and masks
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+        img = read_image(img_path)
+        mask = read_image(mask_path)
+        # instances are encoded as different colors
+        obj_ids = torch.unique(mask)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+        num_objs = len(obj_ids)
 
-        img_path, annotation, obj_labels = self.data[idx], self.annotations[idx], self.obj_labels[idx]
-        N = len(obj_labels)
+        # split the color-encoded mask into a set
+        # of binary masks
+        masks = (mask == obj_ids[:, None, None]).to(dtype=torch.uint8)
 
-        image = Image.open(img_path).convert("RGB")
-        w, h = image.size
+        # get bounding box coordinates for each mask
+        boxes = masks_to_boxes(masks)
 
-        # define requirements for input to model
-        image_id = torch.tensor([idx])
-        labels = torch.as_tensor(obj_labels, dtype=torch.int64)
-        boxes = torch.as_tensor(annotation, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+
+        image_id = idx
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        iscrowd = torch.zeros((N,), dtype=torch.int64)
-        masks = torch.zeros((N, h, w), dtype=torch.uint8)
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
 
-        for i, box in enumerate(np.array(annotation, dtype='int32')):
-            row_s, row_e = box[1], box[3]
-            col_s, col_e = box[0], box[2]
-            masks[i, row_s:row_e, col_s:col_e] = 1
+        # Wrap sample and targets into torchvision tv_tensors:
+        img = tv_tensors.Image(img)
 
         target = {}
-        target["boxes"] = boxes
+        target["boxes"] = tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=F.get_size(img))
+        target["masks"] = tv_tensors.Mask(masks)
         target["labels"] = labels
-        target["masks"] = masks
         target["image_id"] = image_id
         target["area"] = area
         target["iscrowd"] = iscrowd
 
-        if self.transform is not None:
-            image, target = self.transform(image, target)
-        return image, target
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
 
     def __len__(self):
-        return len(self.data)
+        return len(self.imgs)
--- train.py
+++ train.py
@@ -1,31 +1,106 @@
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+from torchvision.transforms import v2 as T
+import keras
 import torch
-import Utils.utils
-import Utils.transforms as T
-from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter()
-from torch.utils.data import Dataset
+
 import CardFieldsDetection
-import Mask_RCNN
 from Utils.engine import train_one_epoch, evaluate
+import Mask_RCNN
+from Utils import  utils
+def get_transform(train):
+    transforms = []
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    transforms.append(T.ToDtype(torch.float, scale=True))
+    transforms.append(T.ToPureTensor())
+    return T.Compose(transforms)
+# load a model pre-trained on COCO
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+
+# replace the classifier with a new one, that has
+# num_classes which is user-defined
+num_classes = 2  # 1 class (person) + background
+# get number of input features for the classifier
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+# replace the pre-trained head with a new one
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+
+
+# load a pre-trained model for classification and return
+# only the features
+backbone = torchvision.models.mobilenet_v2(weights="DEFAULT").features
+# ``FasterRCNN`` needs to know the number of
+# output channels in a backbone. For mobilenet_v2, it's 1280
+# so we need to add it here
+backbone.out_channels = 1280
+
+# let's make the RPN generate 5 x 3 anchors per spatial
+# location, with 5 different sizes and 3 different aspect
+# ratios. We have a Tuple[Tuple[int]] because each feature
+# map could potentially have different sizes and
+# aspect ratios
+anchor_generator = AnchorGenerator(
+    sizes=((32, 64, 128, 256, 512),),
+    aspect_ratios=((0.5, 1.0, 2.0),)
+)
+
+# let's define what are the feature maps that we will
+# use to perform the region of interest cropping, as well as
+# the size of the crop after rescaling.
+# if your backbone returns a Tensor, featmap_names is expected to
+# be [0]. More generally, the backbone should return an
+# ``OrderedDict[Tensor]``, and in ``featmap_names`` you can choose which
+# feature maps to use.
+roi_pooler = torchvision.ops.MultiScaleRoIAlign(
+    featmap_names=['0'],
+    output_size=7,
+    sampling_ratio=2,
+)
+
+# put the pieces together inside a Faster-RCNN model
+model = FasterRCNN(
+    backbone,
+    num_classes=2,
+    rpn_anchor_generator=anchor_generator,
+    box_roi_pool=roi_pooler,
+)
+
+
 # train on the GPU or on the CPU, if a GPU is not available
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 # our dataset has two classes only - background and person
-num_classes = 6+1
+num_classes = 2
 # use our dataset and defined transformations
-dataset = CardFieldsDetection.CardFieldsDetection('Dataset/train/CNIC_UK', 'Dataset_Annotations',
-                                is_Train=True, transform=T.ToTensor())
-dataset_test = CardFieldsDetection.CardFieldsDetection('Dataset/val/CNIC_UK', 'Dataset_Annotations',
-                                is_Train=False, transform=T.ToTensor())
+dataset = CardFieldsDetection.CardFieldsDetection('Dataset', get_transform(train=True))
+dataset_test = CardFieldsDetection.CardFieldsDetection('Dataset', get_transform(train=False))
+
+# split the dataset in train and test set
+indices = torch.randperm(len(dataset)).tolist()
+dataset = torch.utils.data.Subset(dataset, indices[:-50])
+dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
 
 # define training and validation data loaders
 data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=8, shuffle=True, num_workers=4,
-    collate_fn=Utils.utils.collate_fn)
+    dataset,
+    batch_size=2,
+    shuffle=True,
+    num_workers=4,
+    collate_fn=utils.collate_fn
+)
 
 data_loader_test = torch.utils.data.DataLoader(
-    dataset_test, batch_size=8, shuffle=False, num_workers=4,
-    collate_fn=Utils.utils.collate_fn)
+    dataset_test,
+    batch_size=1,
+    shuffle=False,
+    num_workers=4,
+    collate_fn=utils.collate_fn
+)
 
 # get the model using our helper function
 model = Mask_RCNN.get_model_instance_segmentation(num_classes)
@@ -35,46 +110,29 @@ model.to(device)
 
 # construct an optimizer
 params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(params, lr=0.01,
-                            momentum=0.9, weight_decay=0.0005)
-# and a learning rate scheduler
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+optimizer = torch.optim.SGD(
+    params,
+    lr=0.005,
+    momentum=0.9,
+    weight_decay=0.0005
+)
 
-num_epochs = 10
+# and a learning rate scheduler
+lr_scheduler = torch.optim.lr_scheduler.StepLR(
+    optimizer,
+    step_size=3,
+    gamma=0.1
+)
+
+# let's train it for 5 epochs
+num_epochs = 5
 
 for epoch in range(num_epochs):
-    # metric_logger = utils.MetricLogger(delimiter="  ")
     # train for one epoch, printing every 10 iterations
     train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
     # update the learning rate
     lr_scheduler.step()
     # evaluate on the test dataset
-    coco_logger = evaluate(model, data_loader_test, device=device)
+    evaluate(model, data_loader_test, device=device)
 
-    # training metrics
-    # train_loss = metric_logger.meters['loss'].avg
-    # loss_classifier = metric_logger.meters['loss_classifier'].avg
-    # loss_box_reg = metric_logger.meters['loss_box_reg'].avg
-    # loss_mask = metric_logger.meters['loss_mask'].avg
-
-    # evaluation metrics
-    bbox_vals = coco_logger.coco_eval['bbox']
-    bbox_precision = bbox_vals.stats[:3]
-    segm_vals = coco_logger.coco_eval['segm']
-    segm_mAP = segm_vals.stats[:3]
-
-    # tensorboard bookeeping
-    # writer.add_scalar("training loss", train_loss, epoch)
-    # writer.add_scalar("training classifier loss", loss_classifier, epoch)
-    # writer.add_scalar("training bbox regression loss", loss_box_reg, epoch)
-    # writer.add_scalar("training segmentation mask loss", loss_mask, epoch)
-
-    writer.add_scalar("bbox mAP IoU @ 0.5:0.95", bbox_precision[0], epoch)
-    writer.add_scalar("bbox mAP IoU @ 0.5", bbox_precision[1], epoch)
-    writer.add_scalar("bbox mAP IoU @ 0.75", bbox_precision[2], epoch)
-
-    writer.add_scalar("segm mAP IoU @ 0.5:0.95", segm_mAP[0], epoch)
-    writer.add_scalar("segm mAP IoU @ 0.5", segm_mAP[1], epoch)
-    writer.add_scalar("segm mAP IoU @ 0.75", segm_mAP[2], epoch)
-
-writer.flush()
+print("That's it!")
